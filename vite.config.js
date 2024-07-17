e6bd40736eb555a40bb586dbb6e1845de1939d0e
AsifSyeed <asifsyeed666@gmail.com>
2024-07-18 00:42:32 +0600

    Config updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import babel from '@vitejs/plugin-babel';
+import { viteBabel } from 'vite-plugin-babel';
 
 export default defineConfig({
   plugins: [
     vue(),
-    babel({
+    viteBabel({
       babelConfig: {
         plugins: ['@babel/plugin-transform-runtime']
       }
